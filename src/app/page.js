a7cdb86a9b137540fafe3196852dfbfa076d5be8
Jonah King <jonahkingcs@gmail.com>
2026-01-23 10:39:15 +0000

    Add title section design with blue background

--- src/app/page.js
+++ src/app/page.js
@@ -19,38 +19,42 @@ export default function Home() {
   }
 
   return (
-    <div className="display-headings flex min-h-screen items-center justify-center bg-brand-cream font-sans dark:bg-brand-brown">
-      <main className="display-headings flex min-h-screen w-full max-w-3xl flex-col items-center gap-14 py-62 px-16 bg-brand-cream dark:bg-brand-brown sm:items-start">
-        <h1 className="font-extrabold text-brand-blue dark:text-white">
-          Welcome to <br />
-          <span className="text-brand-brown dark:text-brand-orange ">EduRater</span>
-        </h1>
+    <div className="">
+      <main className="min-h-screen flex flex-col items-center justify-center gap-24 bg-brand-cream dark:bg-brand-brown">
+        <section className="display-headings w-full flex flex-col items-center justify-center py-62 bg-brand-blue dark:bg-brand-blue">
+          <h1 className="font-extrabold text-brand-brown dark:text-white">
+            Welcome to <br />
+            <span className="text-brand-cream dark:text-brand-orange">EduRater</span>
+          </h1>
+        </section>
         
-        <div className="w-full flex flex-col gap-3">
-          <form
-            className="w-full flex flex-col gap-3"
-            onSubmit={(e) => {
-              e.preventDefault();
-              onSearch();
-            }}
-          >
-            <input
-              type="text"
-              value={q}
-              onChange={(e) => setQ(e.target.value)}   /* update q state on input change */
-              placeholder="Search for schools..."
-              className="w-full rounded-md border border-brand-brown px-4 py-2 text-brand-blue placeholder:text-brand-brown focus:border-brand-blue focus:outline-none focus:ring-2 focus:ring-brand-blue bg-brand-cream dark:bg-brand-brown dark:border-brand-cream dark:placeholder-brand-cream"
-            />
-
-            <button
-              type="button"
-              onClick={onSearch}    
-              className="self-center rounded-md px-6 py-3 bg-brand-brown dark:bg-brand-cream text-brand-cream dark:text-brand-brown font-bold hover:bg-brand-orange dark:hover:bg-brand-blue hover:text-white dark:hover:text-brand-cream focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
+        <section className="w-full flex flex-col gap-3 max-w-lg">
+          <div>
+            <form
+              className="w-full flex flex-col gap-3"
+              onSubmit={(e) => {
+                e.preventDefault();
+                onSearch();
+              }}
             >
-              Search
-            </button>
-          </form>
-        </div>
+              <input
+                type="text"
+                value={q}
+                onChange={(e) => setQ(e.target.value)}   /* update q state on input change */
+                placeholder="Search for schools..."
+                className="w-full rounded-md border border-brand-brown px-4 py-2 text-brand-blue placeholder:text-brand-brown focus:border-brand-blue focus:outline-none focus:ring-2 focus:ring-brand-blue bg-brand-cream dark:bg-brand-brown dark:border-brand-cream dark:placeholder-brand-cream"
+              />
+
+              <button
+                type="button"
+                onClick={onSearch}    
+                className="self-center rounded-md px-6 py-3 bg-brand-brown dark:bg-brand-cream text-brand-cream dark:text-brand-brown font-bold hover:bg-brand-orange dark:hover:bg-brand-blue hover:text-white dark:hover:text-brand-cream focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
+              >
+                Search
+              </button>
+            </form>
+          </div>
+        </section>
       </main>
     </div>
   );
