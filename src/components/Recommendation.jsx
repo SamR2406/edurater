83c22c6540738da710b1484d4fa9cfafb5a8902e
Jonah King <jonahkingcs@gmail.com>
2026-01-27 16:24:41 +0000

    Fixed branding colours on search recommendations

--- src/components/Recommendation.jsx
+++ src/components/Recommendation.jsx
@@ -1,9 +1,9 @@
 /* school and num are passed as props containing the schools data and its index */
-export default function RecommendationCard({ school, num }) {
+export default function RecommendationCard({ school }) {
     if (!school) return null;   /* prevent rendering if no school data is provided */
 
     return (
-        <div className="display-headings h-full p-6 bg-brand-blue dark:bg-brand-cream border-b-1 border-brand-brown dark:border-brand-blue">
+        <div className="display-headings h-full p-6">
             {/* Display school name as heading */}
             <h2 className="text-lg font-semibold text-brand-cream dark:text-brand-blue">{school.EstablishmentName}</h2>
         </div>
